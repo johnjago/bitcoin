8231e10732ed24b539a831d8d293be74275a11cb
John Jago <johnjago@protonmail.com>
2021-03-29 11:09:50 -0500

    feat: format output to show price in USD

--- src/Main.hs
+++ src/Main.hs
@@ -3,14 +3,23 @@
 module Main where
 
 import Network.HTTP.Simple ( httpBS, getResponseBody )
+import Control.Lens ( preview )
+import Data.Aeson.Lens ( key, _String )
 import qualified Data.ByteString.Char8 as BS
+import Data.Text ( Text )
+import qualified Data.Text.IO as TIO
 
 fetchJSON :: IO BS.ByteString
 fetchJSON = do
   res <- httpBS "https://api.coindesk.com/v1/bpi/currentprice.json"
   return (getResponseBody res)
 
+getRate :: BS.ByteString -> Maybe Text
+getRate = preview (key "bpi" . key "USD" . key "rate" . _String)
+
 main :: IO ()
 main = do
   json <- fetchJSON
-  BS.putStrLn json
+  case getRate json of
+    Nothing -> TIO.putStrLn "Could not find the Bitcoin rate"
+    Just rate -> TIO.putStrLn $ "The current price of Bitcoin in USD is $" <> rate
