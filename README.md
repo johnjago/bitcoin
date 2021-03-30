00e1d2e04cf0cb1b90dbb47cee6f0299f796bde5
John Jago <johnjago@protonmail.com>
2021-03-30 11:35:27 -0500

    feat: choose from USD, GBP, or EUR currencies

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # bitcoin
 
-From the tutorial [Making a small Haskell application](https://functional.christmas/2019/23)
+From the tutorial [Making a small Haskell application](https://functional.christmas/2019/23), with some additional features added.
+
+## Additional features
+
+- Choose from three available currencies for displaying the Bitcoin price. The options are USD, GBP, and EUR.
--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,7 @@ import Control.Lens ( preview )
 import Data.Aeson.Lens ( key, _String )
 import qualified Data.ByteString.Char8 as BS
 import Data.Text ( Text )
+import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
 fetchJSON :: IO BS.ByteString
@@ -14,12 +15,14 @@ fetchJSON = do
   res <- httpBS "https://api.coindesk.com/v1/bpi/currentprice.json"
   return (getResponseBody res)
 
-getRate :: BS.ByteString -> Maybe Text
-getRate = preview (key "bpi" . key "USD" . key "rate" . _String)
+getRate :: Text -> BS.ByteString -> Maybe Text
+getRate currency = preview (key "bpi" . key currency . key "rate" . _String)
 
 main :: IO ()
 main = do
   json <- fetchJSON
-  case getRate json of
-    Nothing -> TIO.putStrLn "Could not find the Bitcoin rate"
-    Just rate -> TIO.putStrLn $ "The current price of Bitcoin in USD is $" <> rate
+  putStrLn "Choose a currency: USD, GBP, or EUR"
+  currency <- getLine
+  case getRate (T.toUpper (T.pack currency)) json of
+    Nothing -> TIO.putStrLn $ "Could not find the Bitcoin rate in " <> (T.toUpper (T.pack currency))
+    Just rate -> TIO.putStrLn $ "The current price of Bitcoin in " <> (T.toUpper (T.pack currency)) <> " is " <> rate
